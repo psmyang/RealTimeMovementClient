7157a420ea4d5b8f98c6f5fd00ada30772fcdbad
psmyang <ymk1234567890ymk@sina.com>
2021-12-15 15:23:36 -0500

    Initial commit

---
+++ Assets/Scripts/NetworkedClientProcessing.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static public class NetworkedClientProcessing
+{
+
+    #region Send and Receive Data Functions
+    static public void ReceivedMessageFromServer(string msg)
+    {
+        Debug.Log("msg received = " + msg + ".");
+
+        string[] csv = msg.Split(',');
+        int signifier = int.Parse(csv[0]);
+
+        // if (signifier == ServerToClientSignifiers.asd)
+        // {
+
+        // }
+        // else if (signifier == ServerToClientSignifiers.asd)
+        // {
+
+        // }
+
+        //gameLogic.DoSomething();
+
+    }
+
+    static public void SendMessageToServer(string msg)
+    {
+        networkedClient.SendMessageToServer(msg);
+    }
+
+    #endregion
+
+    #region Connection Related Functions and Events
+    static public void ConnectionEvent()
+    {
+        Debug.Log("Network Connection Event!");
+    }
+    static public void DisconnectionEvent()
+    {
+        Debug.Log("Network Disconnection Event!");
+    }
+    static public bool IsConnectedToServer()
+    {
+        return networkedClient.IsConnected();
+    }
+    static public void ConnectToServer()
+    {
+        networkedClient.Connect();
+    }
+    static public void DisconnectFromServer()
+    {
+        networkedClient.Disconnect();
+    }
+
+    #endregion
+
+    #region Setup
+    static NetworkedClient networkedClient;
+    static GameLogic gameLogic;
+
+    static public void SetNetworkedClient(NetworkedClient NetworkedClient)
+    {
+        networkedClient = NetworkedClient;
+    }
+    static public NetworkedClient GetNetworkedClient()
+    {
+        return networkedClient;
+    }
+    static public void SetGameLogic(GameLogic GameLogic)
+    {
+        gameLogic = GameLogic;
+    }
+
+    #endregion
+
+}
+
+#region Protocol Signifiers
+static public class ClientToServerSignifiers
+{
+    public const int asd = 1;
+}
+
+static public class ServerToClientSignifiers
+{
+    public const int asd = 1;
+}
+
+#endregion
+
